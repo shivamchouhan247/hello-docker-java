e55f52ec43515f5b3fa23dfc2ff08b15951e1a34
shivamchouhan247 <chouhan2shivam@gmail.com>
2025-12-17 18:17:42 +0530

    simple java program for docker

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+ 
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Docker");
+        System.out.println("Current Time: " + LocalDateTime.now());
+    }
+}
