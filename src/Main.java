9346b63a4daae81d9419085f2557677d4d9db494
shivamchouhan247 <chouhan2shivam@gmail.com>
2025-12-26 20:34:48 +0530

    Containerization of application

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Docker");
+        System.out.println("Hello Dosto kese ho app!");
         System.out.println("Current Time: " + LocalDateTime.now());
     }
 }
